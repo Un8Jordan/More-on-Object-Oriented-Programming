b41f2899c0f6912e94c1cc89e0c0a38a8df1f1a9
Un8Jordan <laxmirathore554@gmail.com>
2025-09-10 20:43:18 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+class IOSString():
+    def _init_(self):
+        self.str1 = ""
+
+        def get_String(self):
+            self.str1 = input("Enter String : ")
+
+            def print_String(self):
+                print("Result is :",self.str1.upper())
+                
+str1 = IOSString()
+
+
+str1.get_String() 
+str1.print_String()
